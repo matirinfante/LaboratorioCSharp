2f91ecf1e3deec8c65ec60c09eb97d081abf0daa
Matias <maty.infante@gmail.com>
2019-09-29 21:42:45 -0300

    adding lab 2 and 3

---
+++ Laboratorio2/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Laboratorio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ejercicio 1
+
+            int n1 = 10, n2 = 20, n3 = 30;
+
+            if (n1 > n2 && n1 > n3)
+            {
+                Console.WriteLine($"N1 es el mayor: {n1}");
+            }
+            else if (n2 > n1 && n2 > n3)
+            {
+                Console.WriteLine($"N2 es el mayor: {n2}");
+            }
+            else if (n3 > n2 && n3 > n1)
+            {
+                Console.WriteLine($"N3 es el mayor: {n3}");
+            }
+
+            #endregion
+
+            #region Ejercicio 2
+
+            int a = -10, b = 2, c = 5;
+
+            if (a > 0 && b > 0)
+            {
+                Console.WriteLine($"a x b = {a * b}");
+            }
+            else if (a > 0 && c > 0)
+            {
+                Console.WriteLine($"a x c = {a * c}");
+            }
+            else if (b > 0 && c > 0)
+            {
+                Console.WriteLine($"b x c = {b * c}");
+            }
+
+            #endregion
+
+            #region Ejercicio3
+
+            String usuario = "Pepito", contraseña = "1234";
+
+            if (usuario.Equals("Pepito") && contraseña.Equals("1234"))
+            {
+                Console.WriteLine("Bienvenido Pepito!");
+            }
+            else if (!(usuario.Equals("Pepito")) & contraseña.Equals("1234"))
+            {
+                Console.WriteLine("Usuario incorrecto");
+            }
+            else if (usuario.Equals("Pepe") & !(contraseña.Equals("1234")))
+            {
+                Console.WriteLine("Contraseña incorrecta");
+            }
+
+            #endregion
+
+            #region Ejercicio 4
+
+            int numero1, numero2, opcion;
+            Console.WriteLine("Ingrese el primer número: ");
+            int.TryParse(Console.ReadLine(), out numero1);
+            Console.WriteLine("Ingrese el segundo número: ");
+            int.TryParse(Console.ReadLine(), out numero2);
+            Console.WriteLine("Ingrese una de las siguientes opciones: ");
+            Console.WriteLine("1 - Suma");
+            Console.WriteLine("2 - Resta");
+            Console.WriteLine("3 - Multiplicación");
+            Console.WriteLine("4 - Division\n");
+            Console.WriteLine("Opción: ");
+            int.TryParse(Console.ReadLine(), out opcion);
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine($"Suma: {numero1 + numero2}");
+                    break;
+                case 2:
+                    Console.WriteLine($"Resta: {numero1 - numero2}");
+                    break;
+                case 3:
+                    Console.WriteLine($"Multiplicación: {numero1 * numero2}");
+                    break;
+                case 4:
+                    Console.WriteLine(numero2 > numero1
+                        ? $"Division: {(double) numero1 / numero2} con resto: {numero1 % numero2}"
+                        : $"División: {numero1 / numero2}");
+
+                    break;
+            }
+
+            Console.WriteLine("Presione una tecla para continuar...");
+            Console.ReadKey();
+
+            #endregion
+        }
+    }
+}
---
+++ Laboratorio3/Program.cs
@@ -0,0 +1,289 @@
+﻿using System;
+
+namespace Laboratorio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ejercicio 1
+
+            Console.WriteLine("EJERCICIO 1");
+            int n = 1;
+            while (n <= 10)
+            {
+                Console.WriteLine(n);
+                n++;
+            }
+
+            #endregion
+
+            #region Ejercicio 2
+
+            Console.WriteLine("EJERCICIO 2");
+            n = 1;
+            while (n <= 10)
+            {
+                Console.WriteLine(n);
+                n += 2;
+            }
+
+            #endregion
+
+            #region Ejercicio 3
+
+            Console.WriteLine("EJERCICIO 3");
+            n = 10;
+            while (n > 0)
+            {
+                Console.WriteLine(n);
+                n--;
+            }
+
+            #endregion
+
+            #region Ejercicio 4
+
+            Console.WriteLine("EJERCICIO 4");
+            n = 1;
+            while (n <= 10)
+            {
+                if (n != 2 && n != 5 && n != 9)
+                {
+                    Console.WriteLine(n);
+                }
+
+                n++;
+            }
+
+            #endregion
+
+            #region Ejercicio 5
+
+            Console.WriteLine("EJERCICIO 5");
+            n = 1;
+            while (n <= 30)
+            {
+                if (n < 10 || n > 20)
+                {
+                    Console.WriteLine(n);
+                }
+
+                n++;
+            }
+
+            #endregion
+
+            #region Ejercicio 6
+
+            Console.WriteLine("EJERCICIO 6");
+            n = 1;
+            int suma = 0;
+            while (n <= 10)
+            {
+                suma += n;
+                n++;
+            }
+
+            Console.WriteLine($"La suma del 1 al 10 es {suma}");
+
+            #endregion
+
+            #region Ejercicio 7
+
+            Console.WriteLine("EJERCICIO 7");
+            n = 1;
+            suma = 0;
+            while (n <= 25)
+            {
+                if (n % 2 == 0)
+                {
+                    suma += n;
+                }
+
+                n++;
+            }
+
+            Console.WriteLine($"La suma de pares del 1 al 25 es {suma}");
+
+            #endregion
+
+            #region Ejercicio 8
+
+            Console.WriteLine("EJERCICIO 8");
+            n = -10;
+            int productoria = 1;
+
+            while (n <= 10)
+            {
+                if (n % 2 != 0)
+                {
+                    productoria *= n;
+                }
+
+                n++;
+            }
+
+            Console.WriteLine($"La productoria es  {productoria}");
+
+            #endregion
+
+            #region Ejercicio 1 - FOR
+
+            Console.WriteLine("EJERCICIO 1 - FOR");
+            for (n = 1; n <= 10; n++)
+            {
+                Console.WriteLine(n);
+            }
+
+            #endregion
+
+            #region Ejercicio 2 - FOR
+
+            Console.WriteLine("EJERCICIO 2 - FOR");
+            for (n = 1; n <= 10; n += 2)
+            {
+                Console.WriteLine(n);
+            }
+
+            #endregion
+
+            #region Ejercicio 3 - FOR
+
+            Console.WriteLine("EJERCICIO 3 - FOR");
+            for (n = 10; n > 0; n--)
+            {
+                Console.WriteLine(n);
+            }
+
+            #endregion
+
+            #region Ejercicio 4 - FOR
+
+            Console.WriteLine("EJERCICIO 4 - FOR");
+            suma = 0;
+            for (n = 1; n <= 10; n++)
+            {
+                if (n % 2 != 0)
+                {
+                    suma += n;
+                }
+            }
+
+            Console.WriteLine($"La suma de los impares de 1 al 10 es: {suma}");
+
+            #endregion
+
+            #region Ejercicio 5 - FOR
+
+            Console.WriteLine("EJERCICIO 5 - FOR");
+            int producto = 1;
+            suma = 0;
+            for (n = 1; n <= 5; n++)
+            {
+                producto *= n;
+                suma += n;
+            }
+
+            Console.WriteLine($"Resultado resta productoria y sumatoria: {producto - suma}");
+
+            #endregion
+
+            #region Ejercicio 6 - FOR
+
+            Console.WriteLine("EJERCICIO 6 - FOR");
+            for (n = 0; n < 4; n++)
+            {
+                Console.WriteLine("@");
+            }
+
+            #endregion
+
+            #region Ejercicio 7 - FOR
+
+            Console.WriteLine("EJERCICIO 7 - FOR");
+            for (n = 1; n <= 5; n++)
+            {
+                Console.WriteLine(n % 2 != 0 ? "@" : "@@");
+            }
+
+            #endregion
+
+            #region Ejercicio 8 - FOR
+
+            Console.WriteLine("EJERCICIO 8 - FOR");
+            string cadena = "";
+            for (n = 1; n <= 5; n++)
+            {
+                cadena += "@";
+                Console.WriteLine(cadena);
+            }
+
+            #endregion
+
+            #region Ejercicio 9 - FOR
+
+            Console.WriteLine("EJERCICIO 9 - FOR");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 5; j > i; j--)
+                {
+                    Console.Write("@");
+                }
+
+                Console.WriteLine();
+            }
+
+            #endregion
+
+            #region Ejercicio 10 - FOR
+
+            Console.WriteLine("EJERCICIO 10 - FOR");
+            cadena = "";
+            for (n = 1; n <= 5; n++)
+            {
+                cadena += "@";
+                Console.WriteLine(cadena);
+            }
+
+            for (int i = 1; i < 5; i++)
+            {
+                for (int j = 5; j > i; j--)
+                {
+                    Console.Write("@");
+                }
+
+                Console.WriteLine();
+            }
+
+            #endregion
+
+            #region Ejercicio 11 - FOR
+
+            Console.WriteLine("EJERCICIO 11 - FOR");
+            int lineas = 5;
+            int mitad = lineas - 2;
+            for (int i = 0; i < lineas; i += 2)
+            {
+                for (int j = lineas; j > i; j--)
+                {
+                    Console.Write("@");
+                }
+
+                Console.WriteLine();
+            }
+
+            for (int i = mitad; i <= lineas; i += 2)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    Console.Write("@");
+                }
+
+                Console.WriteLine();
+            }
+
+            #endregion
+        }
+    }
+}
